1e0720e972ddaaa3605e064dfa67a79ecc8e34dc
vanCopper <vancopperm@gmail.com>
2019-03-25 16:06:59 +0800

    - init

---
+++ Assets/Scripts/Map/Map.cs
@@ -0,0 +1,13 @@
+﻿// **********************************************************************
+// Author: vanCopper
+// Date: 2019/3/25 15:32:45
+// Desc: 
+// **********************************************************************
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Map
+{
+
+}
---
+++ Assets/Scripts/Map/Utils/ParkMillerRNG.cs
@@ -0,0 +1,15 @@
+﻿// **********************************************************************
+// Author: vanCopper
+// Date: 2019/3/25 16:00:03
+// Desc: 
+// **********************************************************************
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+/// <summary>
+/// Park Miller(1988) 随机数发生器
+/// </summary>
+public class ParkMillerRNG
+{
+
+}
