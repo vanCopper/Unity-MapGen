7be0bc96753ae9e3f7d07b508f348941eb8b5d07
vanCopper <vancopperm@gmail.com>
2019-03-25 16:26:34 +0800

    Update ParkMillerRNG.cs

--- Assets/Scripts/Map/Utils/ParkMillerRNG.cs
+++ Assets/Scripts/Map/Utils/ParkMillerRNG.cs
@@ -11,5 +11,56 @@ using System.Collections.Generic;
 /// </summary>
 public class ParkMillerRNG
 {
+    /// <summary>
+    /// 1-0x7FFFFFFF 不可使用0 作为Seed
+    /// </summary>
+    public uint Seed;
 
+    public ParkMillerRNG()
+    {
+        Seed = 1;
+    }
+
+    public uint Nextuint()
+    {
+        return Gen();
+    }
+
+    /// <summary>
+    /// 0-1.0
+    /// </summary>
+    /// <returns></returns>
+    public double NextDouble()
+    {
+        return (Gen() / int.MaxValue);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public uint NextUintRange(double min, double max)
+    {
+        min -= .4999;
+        max += .4999;
+        return (uint)(min + ((max - min) * NextDouble()));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="min"></param>
+    /// <param name="max"></param>
+    /// <returns></returns>
+    public double NextDoubleRange(double min, double max)
+    {
+        return min + ((max - min) * NextDouble());
+    }
+
+    private uint Gen()
+    {
+        return Seed = (Seed * 16807) % int.MaxValue;
+    }
 }
